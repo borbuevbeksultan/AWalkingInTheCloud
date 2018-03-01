f42b9a7b2eb0585e09b5a5f6c9ab18a6a8494bf6
Borbuev Beksultan <bborbuev@spindletop.software>
2018-03-01 20:13:49 +0600

    Optimize import

--- app/src/main/java/kg/iceknight/grazygo/MainActivity.java
+++ app/src/main/java/kg/iceknight/grazygo/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
-import android.widget.Toast;
 
 import kg.iceknight.grazygo.handler.MainButtonHandler;
 import kg.iceknight.grazygo.handler.NotificationButtonHandler;
@@ -14,8 +13,6 @@ import kg.iceknight.grazygo.service.NotificationService;
 
 import static kg.iceknight.grazygo.common.Constants.EXIT_REQUEST_CODE;
 import static kg.iceknight.grazygo.common.Constants.NOTIFICATION_ID;
-import static kg.iceknight.grazygo.common.Constants.PAUSE_REQUEST_CODE;
-import static kg.iceknight.grazygo.common.Constants.PLAY_REQUEST_CODE;
 
 public class MainActivity extends AppCompatActivity {
 
